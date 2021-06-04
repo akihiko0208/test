a8e7e986b9e3859b1773bab4c88a299caa94b6e8
Akihiko Takahashi <aki.jokerprice@gmail.com>
2021-06-04 20:56:58 +0900

    Add memory leak code.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main(void) {
         int a, b;
+        char* p = malloc(100);
         printf("hello world.\n");
         if (a == 0) {
                 return 1;
         }
+        
         return 0;
 }
